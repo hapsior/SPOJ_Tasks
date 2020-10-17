2e16cfabb49f68a54f4525741a8a7d4d692fd3b9
Damian Hapka <dhapka@outlook.com>
2020-10-17 16:14:33 +0200

    PrimeNumbers

---
+++ SPOJ/PrimeNumbers/Prime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPOJ
+{
+    class Prime
+    {
+        public static void IsPrime(int n)
+        {
+            int l = 0;
+
+            if (n == 1)
+            {
+                Console.WriteLine($"{n} NO");
+            }
+            if (n == 2)
+            {
+                Console.WriteLine($"{n} YES");
+            }
+            for (int x = 2; x < n; x++)
+            {
+                if (n % x == 0)
+                {
+                    Console.WriteLine($"{n} NO");
+                    l = n;
+                    break;
+                }
+            }
+            if (n != l)
+            {
+                Console.WriteLine($"{n} YES");
+            }
+        }
+    }
+}
---
+++ SPOJ/PrimeNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SPOJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add range of number. Script always start from 1");
+            int n = Int32.Parse(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Prime.IsPrime(i);
+            }
+
+
+
+
+        }
+        
+    }
+}
+
+
